4ee12bd6e541a7660df49b58f20962edd1c53d20
rudzzz <rudsolza13@gmail.com>
2022-06-29 22:19:04 -0300

    arquivo de perguntas

---
+++ data.py
@@ -0,0 +1,136 @@
+question_data = [
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A Grande Muralha da China é visível da lua.",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Rio de Janeiro é a capital do Brasil",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]},
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "O francês é uma língua oficial no Canadá.",
+        "correct_answer": "Verdadeiro",
+        "incorrect_answers": ["Falso"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Os touros são atraídos pela cor vermelha.",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A cor laranja tem esse nome por causa da fruta.",
+        "correct_answer": "Verdadeiro",
+        "incorrect_answers": ["Falso"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Plutão é um planeta.",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Adolf Hitler nasceu na Austrália.",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+         "question": "Quando você chora no espaço, suas lágrimas grudam em seu rosto.",
+        "correct_answer": "Verdadeiro",
+         "incorrect_answers": ["Falso"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "O Brasil é hexacampeão de futebol.",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+         "question": "O monóxido de dihidrogênio foi banido devido a riscos à saúde após ser descoberto "
+                     "em 1983 dentro de piscinas e água potável.",
+         "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A lua é um planeta",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "O Animal Nacional da Escócia é o Unicórnio.",
+        "correct_answer": "Verdadeiro",
+        "incorrect_answers": ["Falso"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A bandeira nacional do Vietnã é uma estrela vermelha na frente de um fundo amarelo.",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Um pasodoble é um tipo de molho de macarrão italiano.",
+        "correct_answer": "Falso",
+        "incorrect_answers": ["Verdadeiro"]
+    },
+
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "O pingue-pongue surgiu na Inglaterra",
+        "correct_answer": "Verdadeiro",
+        "incorrect_answers": ["Falso"]
+     }
+]
+
